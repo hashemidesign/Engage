04f1e7b9fff7933300a2fb5d5163a63f1a5b1155
Mohsen Hashemi <m.hashemi.code@gmail.com>
2020-12-17 21:35:48 +0330

    add image upload

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_wtf import FlaskForm
-from wtforms import StringField, FileField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length
-
+from flask_wtf.file import FileAllowed, FileField
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///engage.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'afsfvskjdfnskjdvhhskhdksndv'
+app.config['UPLOADED_PHOTOS_DEST'] = 'images'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+configure_uploads(app, photos)
 
 
 class User(db.Model):
@@ -31,7 +35,7 @@ class RegisterForm(FlaskForm):
     name = StringField('Full Name', validators=[InputRequired('A full name is required'), Length(max=100, message='Your name can\'t be more than 100 characters')])
     username = StringField('Username', validators=[InputRequired('A username is required'), Length(max=30, message='Your username can\'t be more than 30 characters')])
     password = PasswordField("Password", validators=[InputRequired('A password is required')])
-    image = FileField()
+    image = FileField(validators=[FileAllowed(IMAGES, 'Only images are accepted')])
     submit = SubmitField('Register')
 
 
@@ -50,9 +54,14 @@ def timeline():
     return render_template('timeline.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        image_filename = photos.save(form.image.data)
+        image_url = photos.url(image_filename)
+
+        return f'<h1>name: {form.name.data}, username: {form.username.data}, password: {form.password.data}, image_url: {image_url}'
     return render_template('register.html', form=form)
 
 
